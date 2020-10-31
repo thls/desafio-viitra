473a0e9190a04a4a6fec3d4927e8f5b4f50ddff2
thls <thls@cin.ufpe.br>
2020-10-31 12:57:40 -0300

    layout pagina inicial e configuracao da sua rota

--- desafio-app/app/controllers/application_controller.rb
+++ desafio-app/app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery only: []
+  def index
+    render 'layouts/application'
+  end
 end
--- desafio-app/config/routes.rb
+++ desafio-app/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'application#index'
   namespace 'api' do
       resources :usuarios, only: [:index, :show, :create, :destroy, :update]
       get 'usuarios/search/findByName/:name' => 'usuarios#findByName'
