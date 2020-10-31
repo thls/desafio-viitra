c2b3d611505508189c9840f6f54f515e4bf202cd
thls <thls@cin.ufpe.br>
2020-10-31 11:59:16 -0300

    implementacao e configuracao do metodo 'create'

--- desafio-app/app/controllers/api/usuarios_controller.rb
+++ desafio-app/app/controllers/api/usuarios_controller.rb
@@ -16,5 +16,43 @@ module Api
                 render json: {status: 'fail', message: e},status: :ok
             end
         end
+
+        # Criar um novo usuario
+        def create
+            begin
+                if params[:data_nascimento] == nil
+                    return render json: {status: 'fail', message: "Date invalid."},status: :ok
+                end
+                Date.parse(params[:data_nascimento]);
+                
+                usuario = Usuario.new(usuario_params);
+                telefone = Telefone.new(telefone_params);
+                usuario.telefone = telefone;
+                endereco = Endereco.new(endereco_params);
+                usuario.endereco = endereco;
+
+                telefone.save! if telefone.invalid?
+                endereco.save! if endereco.invalid?
+
+                usuario.save!
+                render json: {status: 'success', message: "Usuario registrado com sucesso.", data: usuario}, status: :ok
+            rescue Exception => e
+                render json: {status: 'fail', message: e}, status: :ok
+            end    
+        end
+
+        # Filtrando parametros
+        private
+        def usuario_params
+            params.permit(:nome, :cpf, :email, :data_nascimento)
+        end
+        private
+        def telefone_params
+            params.require(:telefone).permit(:numero)
+        end
+        private
+        def endereco_params
+            params.require(:endereco).permit(:cep, :cidade, :estado)
+        end
     end
 end
--- desafio-app/config/routes.rb
+++ desafio-app/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   namespace 'api' do
-      resources :usuarios, only: [:index, :show]
+      resources :usuarios, only: [:index, :show, :create]
   end
 end
