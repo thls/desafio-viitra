bb31d56f1040ccd93d86d9b20447b099e5cd1633
thls <thls@cin.ufpe.br>
2020-10-31 13:09:26 -0300

    configurando as rotas e setando respectivos controllers

--- desafio-app/app/assets/javascripts/app.js
+++ desafio-app/app/assets/javascripts/app.js
@@ -1,1 +1,18 @@
 var app = angular.module('desafio-app', ['ngRoute', 'templates']);
+
+app.config(function($routeProvider, $locationProvider){
+    $locationProvider.html5Mode({
+    enabled: true,
+    requireBase: false
+  });
+    $routeProvider
+    .when("/cadastrar", {
+        templateUrl: 'cadastro/_cadastro.html',
+        controller: 'CadastroController'
+    })
+    .when("/usuarios", {
+        templateUrl: 'usuarios/_usuarios.html',
+        controller: 'UsuariosController'
+    })
+    .otherwise({redirectTo: '/'});
+});
